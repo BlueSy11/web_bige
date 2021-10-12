3718fb63da7d4b758b5033a4bcfde2d748b28b66
hexiao <1452762537@qq.com>
2021-10-12 22:54:09 +0800

    完成了主页功能

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -1,4 +1,20 @@
 $.ajaxPrefilter(function(option) {
     option.url = 'http://api-breakingnews-web.itheima.net' + option.url;
     console.log(option.url);
+
+    if (option.url.indexOf('/my') !== -1) {
+        option.header = {
+            Authorization: localStorage.getItem('token') || ''
+        }
+    }
+
+    option.complete = function(res) {
+        if (res.responseJSON.status === 1 && res.responseJSON.message === "身份认证失败") {
+            localStorage.removeItem('token');
+            location.href = '/login.html';
+        }
+    }
 })
+
+
+// 唯有权限的请求统一设置header请求头
